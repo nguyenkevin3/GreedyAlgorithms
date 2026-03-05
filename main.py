7a0afb5751a2a360848dbc495ff5014b8b007d08
nguyenkevin3 <148578177+nguyenkevin3@users.noreply.github.com>
2026-03-05 17:07:57 -0500

    Implemented OPTFF

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def fifo(k, requests):
     return number_of_misses
 
 
+
 def lru(k, requests):
     # In order from least recently used to most recently used
     cache = OrderedDict()
@@ -32,22 +33,72 @@ def lru(k, requests):
     number_of_misses = 0
 
     for request in requests:
-        if request not in cache:
-            if len(cache) == k:
+        if request not in cache: # Cache miss
+            if len(cache) == k: # If full, evict
                 cache.popitem(last=False)  # Remove first item (least recently used)
 
             number_of_misses += 1
 
             # Insert item, arbitrary value
             cache[request] = 0
-        else:
+        else: # Cache hit, update most recently used
             cache.move_to_end(request)  # End side is most recently used
 
     return number_of_misses
 
 
+
+# Find the request that is farthest in the future among items in the cache
 def optff(k, requests):
-    pass
+    # To quickly check if item is present in cache
+    s = set()
+
+    # Array to hold items
+    cache = []
+
+    number_of_misses = 0
+
+    # Index and value in requests list
+    for index, request in enumerate(requests):
+        if request not in s: # Cache miss
+            number_of_misses += 1
+            if len(cache) == k: # If full, replace with new item
+                farthestInRequestList = index
+                result = -1  # Index of cache item to evict
+
+                # For each item in the cache
+                for i in range(len(cache)):
+                    j = index
+
+                    # Iterate through requests list starting from index to scan future requests
+                    while j < len(requests):
+                        if cache[i] == requests[j]:  # Only amongst items currently in the cache
+                            if j > farthestInRequestList:
+                                farthestInRequestList = j
+                                result = i
+                            break
+                        j += 1
+
+                    # If cache item is never requested again, that is the item to evict
+                    if j == len(requests):
+                        result = i
+                        break
+
+                if result == -1:
+                    result = 0
+
+
+                s.remove(cache[result])
+
+                cache[result] = request
+                s.add(request)
+
+            else:
+                # Insert item
+                cache.append(request)
+                s.add(request)
+
+    return number_of_misses
 
 
 
@@ -56,15 +107,15 @@ def main():
 
     # Reads entire file and splits by whitespace into a list of strings
     with open(filename) as f:
-        input = f.read().split()
+        userinput = f.read().split()
 
-    k = int(input[0])  # Cache capacity
-    m = int(input[1])  # Number of requests
+    k = int(userinput[0])  # Cache capacity
+    m = int(userinput[1])  # Number of requests
 
     requests = [] # Sequence of integer IDs
 
     for i in range(m):
-        requests.append(int(input[2 + i]))
+        requests.append(int(userinput[2 + i]))
 
     FIFO_misses = fifo(k, requests)
 
